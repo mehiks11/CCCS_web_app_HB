71099399f11a76b91d86ac745af3bc57c399a42d
mehiks11 <mpatel3105@gmail.com>
2021-08-21 16:56:02 -0400

    updates

---
+++ check_csv.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def check_csv(df):
+    # add check for column values here! and then use this link: https://www.youtube.com/watch?v=mBKKZN1MMBM&list=PLF2JzgCW6-YY_TZCmBrbOpgx5pSNBD0_L&index=18
+    # to show error if the csv file is not proper.
+    pass
